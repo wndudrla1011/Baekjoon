e8f7b50e68ebebb15f5b387a623e501c2f96a173
wndudrla1011 <wndudrla1011@naver.com>
2023-01-26 12:55:56 +0900

    solved 수들의 합 5(2018)

---
+++ src/main/java/two_pointer/p2018/Main2.java
@@ -0,0 +1,64 @@
+package two_pointer.p2018;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+public class Main2 {
+
+    public void solution() throws Exception {
+        int n = nextInt();
+
+        int cnt = 0;
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            for (int j = i; j <= n; j++) {
+                sum += j;
+                if (sum > n) break;
+                else if (sum == n) {
+                    cnt++;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+    public static void main(String[] args) throws Exception {
+        initFI();
+        new Main2().solution();
+    }
+
+    private static final int DEFAULT_BUFFER_SIZE = 1 << 16;
+    private static DataInputStream inputStream;
+    private static byte[] buffer;
+    private static int curIdx, maxIdx;
+
+    private static void initFI() {
+        inputStream = new DataInputStream(System.in);
+        buffer = new byte[DEFAULT_BUFFER_SIZE];
+        curIdx = maxIdx = 0;
+    }
+
+    private static int nextInt() throws IOException {
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        boolean neg = (c == '-');
+        if (neg) c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (neg) return -ret;
+        return ret;
+    }
+
+    private static byte read() throws IOException {
+        if (curIdx == maxIdx) {
+            maxIdx = inputStream.read(buffer, curIdx = 0, DEFAULT_BUFFER_SIZE);
+            if (maxIdx == -1) buffer[0] = -1;
+        }
+        return buffer[curIdx++];
+    }
+
+}
