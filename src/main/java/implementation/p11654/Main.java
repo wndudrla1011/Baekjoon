a5ea1c8e36d29a88417dc2964ae8b3acb70e1e46
wndudrla1011 <wndudrla1011@naver.com>
2023-02-04 20:13:32 +0900

    solved 아스키 코드(11654)

---
+++ src/main/java/implementation/p11654/Main.java
@@ -0,0 +1,12 @@
+package implementation.p11654;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        char c = (char) System.in.read();
+        System.out.println((int) c);
+    }
+
+}
