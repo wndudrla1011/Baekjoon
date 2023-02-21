052c03c6028348ad471e6bd690533facc647cde4
wndudrla1011 <wndudrla1011@naver.com>
2023-02-21 14:30:48 +0900

    solved 합 구하기(11441)

---
+++ src/main/java/two_pointer/p11441/Main.java
@@ -0,0 +1,42 @@
+package two_pointer.p11441;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int N = readInt();
+
+        int[] arr = new int[N + 1];
+        for (int i = 1; i <= N; i++) arr[i] = readInt() + arr[i - 1];
+
+        int M = readInt();
+        int sum;
+        while (M-- > 0) {
+            int i = readInt();
+            int j = readInt();
+
+            sum = arr[j] - arr[i - 1];
+
+            sb.append(sum).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+
+    static int readInt() throws IOException {
+        int n = 0;
+        boolean isNegative = false;
+        while (true) {
+            int input = System.in.read();
+            if (input <= 32) {
+                return isNegative ? n * -1 : n;
+            } else if (input == '-')
+                isNegative = true;
+            else
+                n = (n << 3) + (n << 1) + (input & 15);
+        }
+    }
+
+}
