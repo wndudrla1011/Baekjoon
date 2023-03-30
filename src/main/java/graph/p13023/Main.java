1cb0fd55add5acc2ffaa14bcb8ceed5892c057ac
wndudrla1011 <wndudrla1011@naver.com>
2023-03-30 17:30:18 +0900

    solved ABCDE(13023)

---
+++ src/main/java/graph/p13023/Main.java
@@ -0,0 +1,58 @@
+package graph.p13023;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static ArrayList<Integer>[] A;
+    static boolean[] visited;
+    static int N, M;
+    static boolean arrive; //도착 확인 변수
+
+    public static void main(String[] args) throws IOException {
+        N = readInt();
+        M = readInt();
+
+        A = new ArrayList[N];
+        visited = new boolean[N];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = new ArrayList<>();
+        }
+
+        while (M-- > 0) {
+            int s = readInt();
+            int e = readInt();
+            A[s].add(e);
+            A[e].add(s);
+        }
+
+        for (int i = 0; i < N; i++) {
+            DFS(i, 1);
+            if (arrive) break;
+        }
+
+        System.out.println(arrive ? 1 : 0);
+    }
+
+    static void DFS(int now, int depth) {
+        if (depth == 5 || arrive) { //깊이 5까지 도달한 적이 있다면 종료
+            arrive = true;
+            return;
+        }
+        visited[now] = true;
+        for (int i : A[now]) {
+            if (!visited[i])
+                DFS(i, depth + 1);
+        }
+        visited[now] = false;
+    }
+
+    static int readInt() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+
+}
